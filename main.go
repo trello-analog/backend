1ae47c4cddcac2c28efc23d2700dcb8d1cebfe21
maksimkudryavtsev <maxim.kudryavtstev@omega-r.com>
2020-08-31 13:37:49 +0300

    initial commit

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	response := fmt.Sprintf("Product %s", id)
+	fmt.Fprint(w, response)
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	router := mux.NewRouter()
+	router.HandleFunc("/products/{id}", productsHandler)
+	http.Handle("/", router)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", nil)
 }
